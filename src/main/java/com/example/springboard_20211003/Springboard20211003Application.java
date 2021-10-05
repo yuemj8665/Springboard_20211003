ee8b3dbae129ddc98c93b33a376f50dbcf719a13
MJMJ92 <yuemjmahu@gmail.com>
2021-10-05 23:00:14 +0900

    2021-10-05 MapperScan

--- src/main/java/com/example/springboard_20211003/Springboard20211003Application.java
+++ src/main/java/com/example/springboard_20211003/Springboard20211003Application.java
@@ -1,13 +1,13 @@
 package com.example.springboard_20211003;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.example.springboard_20211003")
 public class Springboard20211003Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Springboard20211003Application.class, args);
     }
-
 }
