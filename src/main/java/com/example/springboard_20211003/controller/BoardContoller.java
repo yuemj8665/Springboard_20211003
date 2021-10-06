6c370ccdf0a7601bcbd912b82214d83c5b79f8b8
MJMJ92 <yuemjmahu@gmail.com>
2021-10-06 22:48:06 +0900

    2021-10-06 thymeleaf error ...ing

--- src/main/java/com/example/springboard_20211003/controller/BoardContoller.java
+++ src/main/java/com/example/springboard_20211003/controller/BoardContoller.java
@@ -9,25 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/board/**")
 public class BoardContoller {
-
-    private final BoardService boardService;
-
-    public BoardContoller(BoardService boardService) {
-        this.boardService = boardService;
-    }
-
+    private BoardService boardService;
 
     @GetMapping("/hello")
     public String Hello() {
-        return "/boards/hello";
+        return "boards/hello";
     }
 
     @GetMapping("/test")
     public String test(Model model) {
         model.addAttribute("cnt", boardService.boardCount());
+        model.addAttribute("cnt", 1);
         model.addAttribute("test", boardService.boardList());
 
-        return "/boards/hello";
+        return "boards/hello";
     }
 
 }
--- src/main/java/com/example/springboard_20211003/domain/Board.java
+++ src/main/java/com/example/springboard_20211003/domain/Board.java
@@ -1,12 +1,16 @@
 package com.example.springboard_20211003.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Board {
     private Long boardId;
     private String title;
