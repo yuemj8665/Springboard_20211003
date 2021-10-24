21e34078a99bce3f122cfb219e38d1f61904107c
MJMJ92 <yuemjmahu@gmail.com>
2021-10-24 19:25:45 +0900

    2021-10-24 springboard practice

--- src/main/java/com/example/springboard_20211003/controller/Board_userController.java
+++ src/main/java/com/example/springboard_20211003/controller/Board_userController.java
@@ -4,6 +4,7 @@ import com.example.springboard_20211003.domain.Board_user;
 import com.example.springboard_20211003.service.Board_userService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class Board_userController {
     @PostMapping("/createUser")
     public String createUser(Board_user user) {
         service.createUser(user);
-        return "redirect:/board/main";
+        return "redirect:/board/loginPage";
     }
 
     // 로그인 페이지
@@ -26,11 +27,16 @@ public class Board_userController {
         return "boards/loginPage";
     }
 
+    // 로그인 기능 구현
+    @PostMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("loginValueTest","123123123");
+        return "redirect:boards/main";
+    }
+
     // 회원 가입 페이지
     @GetMapping("/createUserPage")
     public String createUserPage() {
         return "boards/createUserPage";
     }
-
-
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,4 @@
-CREATE TABLE tbl_board
-(
-    boardId  Long auto_increment,
-    title    varchar(30) not null,
-    content varchar(30) not null,
-    name     varchar(30) not null,
-    read     integer default 0,
-    primary key (boardId)
-);
+-- 유저 테이블
 CREATE TABLE board_user
     (
         id          varchar(30) not null,
@@ -17,6 +9,37 @@ CREATE TABLE board_user
         primary key (id)
 );
 
+-- 글 테이블
+CREATE TABLE tbl_board
+(
+    boardId  Long auto_increment,
+    title    varchar(30) not null,
+    content varchar(30) not null,
+    name     varchar(30) not null,
+    read     integer default 0,
+    primary key (boardId),
+    FOREIGN KEY (name)
+    REFERENCES tbl_board (name)
+);
+
+-- 유저 기초 데이터 추가
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id1','password1','name1','1','address1');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id2','password2','name2','2','address2');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id3','password3','name3','3','address3');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id4','password4','name4','4','address4');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id5','password5','name5','5','address5');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id6','password6','name6','6','address6');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id7','password7','name7','7','address7');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id8','password8','name8','8','address8');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id9','password9','name9','9','address9');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id10','password10','name10','10','address10');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id11','password11','name11','11','address11');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id12','password12','name12','12','address12');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id13','password13','name13','13','address13');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id14','password14','name14','14','address14');
+INSERT INTO board_user(id,password,name,age,address) VALUES ('id15','password15','name15','15','address15');
+
+-- 글 기초 데이터 추가
 INSERT INTO tbl_board(title, content, name) VALUES ('title1','content1','name1');
 INSERT INTO tbl_board(title, content, name) VALUES ('title2','content2','name2');
 INSERT INTO tbl_board(title, content, name) VALUES ('title3','content3','name3');
